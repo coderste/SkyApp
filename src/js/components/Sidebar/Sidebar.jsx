48510ee3aa684e9de0480af4108736f1b29fc828
Stephen <me@stephenhinett.co.uk>
2018-07-29 23:58:52 +0100

    Removed sidebar text

--- src/js/components/Sidebar/Sidebar.jsx
+++ src/js/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <div className="sidebar__container">
-      <div className="sidebar__content">
-        <h3 className="sidebar__title">
-Sidebar
-        </h3>
-      </div>
-    </div>
-  </aside>
-);
+const Sidebar = () => <aside className="sidebar" />;
 
 export default Sidebar;
